00f6c819bdc2d74a40e55b9e46ab0d9bc16109f0
Kenan Kadic <ken.kadic@outlook.com>
2020-05-30 23:22:38 -0500

    added Description

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,24 +1,34 @@
 package org.launchcode.codingevents.models;
 
 public class Event {
-        private String name;
 
-        public Event(String name) {
-            this.name = name;
-        }
+    private String name;
+    private String description;
 
-        public String getName() {
-            return name;
-        }
+    public Event(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        @Override
-        public String toString() {
-            return name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
 
